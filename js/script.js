bbec078c17c2f4d3b01601e0599305dd8af802d3
Matt Thomas <me@matthewathomas.com>
2017-09-16 22:49:35 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Polyfill polymer
+(function() {
+	if (
+		'registerElement' in document
+		&& 'import' in document.createElement('link')
+		&& 'content' in document.createElement('template')) {
+		// platform is good!
+	} else {
+		// polyfill the platform!
+		var e = document.createElement('script');
+		e.src = '/bower_components/webcomponentsjs/webcomponents-lite.js';
+		document.body.appendChild(e);
+	}
+})();
